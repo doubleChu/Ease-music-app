205a006baf091574ce1224db80757fc19f86ef59
Wenson Chu <nomoredays@foxmail.com>
2022-03-22 10:03:57 +0800

    add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       App
     </div>
+    </BrowserRouter>
   );
 }
 
